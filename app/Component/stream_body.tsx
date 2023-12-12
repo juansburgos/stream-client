64da673049912596bcf19dae6c3a3eb396b84b71
Juan S. Burgos <jsburgos@fi.uba.ar>
2023-12-12 00:52:49 -0300

    funciona con un mini bug.

--- app/Component/stream_body.tsx
+++ app/Component/stream_body.tsx
@@ -3,101 +3,62 @@
 
 import {useEffect} from "react";
 import { useSearchParams } from "next/navigation";
+import {arrayBuffer} from "stream/consumers";
+import {isUint8Array} from "util/types";
 
 export default function StreamBody() {
     const params = useSearchParams()
-    var bool = false
-    var bool2 = false
-    var i = 0
-    useEffect(() => {
+    var bool = true
+    var ws = null, mediaRecorder;
+    const id  = params.get('id')
+    var mystream
+    function connect() {
+        // if(bool) return
+        let randomId = Math.floor(Math.random() * 10000) + 1;
+        ws = new WebSocket("ws://localhost:8080/rooms/" + id + "?Id=" + randomId + "&username=frontStreamer");
+        ws.binaryType = "arraybuffer"
+        ws.onmessage = (e) => {
+            if(ws.readyState != ws.OPEN)
+                return
+            console.log(e.data.byteLength)
+            if (e.data.byteLength != 20)
+                return
+            mystream.getVideoTracks()[0].stop()
+            mystream.getAudioTracks()[0].stop()
+            ws.send(new Uint8Array([1, 2, 3, 4, 5]).buffer)
+            streamCamera()
+        }
+    }
+
+
+    function streamCamera() {
         navigator.mediaDevices.getUserMedia({video: true, audio: true}).then(stream => {
             // @ts-ignore
-            var ws = null, mediaRecorder;
-
+            mystream = stream
             var options = {
                 mimeType: "video/webm;codecs=opus, vp8",
                 audioBitsPerSecond: 128000,
                 videoBitsPerSecond: 2500000,
             };
-
-            function connect() {
-                if(bool) return
-                const id  = params.get('id')
-                console.log("id ",id)
-                let randomId = Math.floor(Math.random() * 10000) + 1;
-                ws = new WebSocket("ws://localhost:8080/rooms/" + id + "?Id=" + randomId + "&username=frontStreamer");
-                ws.binaryType = "arraybuffer"
-                mediaRecorder = new MediaRecorder(stream, options);
-                mediaRecorder.ondataavailable = function (e) {
-
-                    if (e.data && e.data.size > 0) {
-                        e.data.arrayBuffer().then(buffer => {
-                            // @ts-ignore
-                            i += 1
-                            if (i > 20 && i < 25) return
-                            ws.send(buffer)
-                        })
-                    }
-                }
-                mediaRecorder.start(1000);
-                bool = true
-            }
-
-            var sourceBuffer,ws2 = null, video, media;
-            var queue = [];
-
-            function handleStream(e) {
-                const buffer = e.data
-                console.log("Appending: ", buffer.size)
-                if (sourceBuffer.updating) {
-                    queue.push(buffer)
-                } else {
-                    sourceBuffer.appendBuffer(buffer)
+            mediaRecorder = new MediaRecorder(stream, options);
+            mediaRecorder.ondataavailable = function (e) {
+                if (e.data && e.data.size > 0) {
+                    e.data.arrayBuffer().then(buffer => {
+                        ws.send(buffer)
+                    })
                 }
             }
-
-            function createVideo() {
-                media = new MediaSource();
-                media.onsourceopen=function() {
-                    sourceBuffer = media.addSourceBuffer("video/webm;codecs=opus, vp8");
-                    sourceBuffer.onupdate = e => {
-                        if (queue.length > 0 && !sourceBuffer.updating) {
-                            sourceBuffer.appendBuffer(queue.shift())
-                        }
-                    }
-                    sourceBuffer.mode = "segments"
-                }
-                video = document.getElementById('videoID')
-                video.oncanplay = e => video.play()
-                video.src = URL.createObjectURL(media);
-                video.onerror = e => {
-                    console.log("ERROR VIDEO")
-                }
-            }
-
-            /*function connect2(){
-                const id  = params.get('id')
-                console.log("id v ",id)
-                if (bool2) {
-                    return
-                }
-                let randomId = Math.floor(Math.random() * 10000) + 1;
-                ws2 = new WebSocket("ws://localhost:8080/rooms/"+id+"?Id=" + randomId + "&username=frontViewer");
-                ws2.binaryType = "arraybuffer"
-                ws2.onopen = createVideo
-                ws2.onmessage = handleStream
-                bool2 = true
-            }*/
-
-            connect()
-            //connect2()
+            mediaRecorder.start(50);
         })
+    }
 
+    useEffect(() => {
+        connect()
+        streamCamera()
     })
 
     return (
-        <div>
-            <video style={{maxWidth:1200, maxHeight:800}} id="videoID"></video>
+        <div> HOLA
         </div>
     )
 }
--- app/Component/viewer_body.tsx
+++ app/Component/viewer_body.tsx
@@ -7,105 +7,63 @@ import { useSearchParams } from "next/navigation";
 export default function ViewerBody() {
     const params = useSearchParams()
 
-    // let bool = false
-    // let bool2 = false
     let sourceBuffer, video, ws2 = null, media;
     let queue = [];
 
+    function connect2(){
+        const id  = params.get('id')
+        console.log("id v ",id)
+        let randomId = Math.floor(Math.random() * 10000) + 1;
+        ws2 = new WebSocket("ws://localhost:8080/rooms/"+id+"?Id=" + randomId + "&username=frontViewer");
+        ws2.binaryType = "arraybuffer"
+        ws2.onmessage = handleStream
+    }
+
+    function handleStream(e) {
+        const buffer = e.data
+        if (!buffer) return
+        if (buffer.byteLength == 5)
+            createVideo()
+        if(buffer.byteLength == 20)
+            return
+        if (media.readyState != 'open')
+            return
+        console.log("Buffer: ", buffer)
+        if (sourceBuffer.updating) {
+            queue.push(buffer)
+        } else {
+            sourceBuffer.appendBuffer(buffer)
+        }
+    }
+
+    function createVideo() {
+        media = new MediaSource();
+        media.onsourceopen = () => {
+            sourceBuffer = media.addSourceBuffer("video/webm;codecs=opus, vp8");
+            sourceBuffer.onupdateend = () => {
+                if (queue.length > 0 && !sourceBuffer.updating) {
+                    sourceBuffer.appendBuffer(queue.shift())
+                }
+            }
+            sourceBuffer.mode = "segments"
+        }
+        video = document.getElementById('videoID')
+        video.oncanplay = () => video.play()
+        video.src = URL.createObjectURL(media);
+        video.onerror = e => {
+            console.log(e)
+            createVideo()
+        }
+    }
+
     useEffect(() => {
-        // navigator.mediaDevices.getUserMedia({video: true, audio: true}).then(stream => {
-            // @ts-ignore
-            /*var ws = null, mediaRecorder;
-
-            var options = {
-                mimeType: "video/webm;codecs=opus, vp8",
-                bitsPerSecond: 5000 //quality
-            };
-
-
-            function connect() {
-                let randomId = Math.floor(Math.random() * 10000) + 1;
-                ws = new WebSocket("ws://localhost:8080/rooms/0?Id=" + randomId + "&username=frontStreamer");
-                ws.binaryType = "arraybuffer"
-                mediaRecorder = new MediaRecorder(stream, options);
-                mediaRecorder.ondataavailable = function (e) {
-
-                    if (e.data && e.data.size > 0) {
-                        e.data.arrayBuffer().then(buffer => {
-                            // @ts-ignore
-                            ws.send(buffer)
-                            console.log("Sent: ", e.data.size)
-                        })
-                    }
-                }
-                mediaRecorder.start(300);
-                bool = true
-            }
-            */
-            // let sourceBuffer,ws2 = null, video, media;
-            // let queue = [];
-
-            function handleStream(e) {
-                if (media.readyState != 'open'){
-                    console.log("not open")
-                    return
-                }
-                const buffer = e.data
-                console.log("Buffer: ", buffer)
-                if (!buffer) return
-                if (sourceBuffer.updating) {
-                    queue.push(buffer)
-                } else {
-                    sourceBuffer.appendBuffer(buffer)
-                }
-            }
-
-            function createVideo() {
-                // if (bool2) return
-                media = new MediaSource();
-                media.onsourceopen = () => {
-                    // setTimeout(() => {
-                    //
-                    // }, 500)
-                    sourceBuffer = media.addSourceBuffer("video/webm;codecs=opus, vp8");
-                    sourceBuffer.onupdateend = () => {
-                        if (queue.length > 0 && !sourceBuffer.updating) {
-                            sourceBuffer.appendBuffer(queue.shift())
-                        }
-                    }
-                    sourceBuffer.mode = "segments"
-                }
-                video = document.getElementById('videoID')
-                video.oncanplay = e => video.play()
-                video.src = URL.createObjectURL(media);
-                video.onerror = e => {
-                    console.log("ERROR VIDEO:", e)
-                }
-                // bool2 = true
-            }
-
-            function connect2(){
-                const id  = params.get('id')
-                console.log("id v ",id)
-                // if (bool2) {
-                //     return
-                // }
-                let randomId = Math.floor(Math.random() * 10000) + 1;
-                ws2 = new WebSocket("ws://localhost:8080/rooms/"+id+"?Id=" + randomId + "&username=frontViewer");
-                ws2.binaryType = "arraybuffer"
-                // ws2.onopen = createVideo
-                ws2.onmessage = handleStream
-            }
-            //connect()
             createVideo()
             connect2()
     })
 
-    // })
-
     return (
         <div>
-            <video style={{maxWidth:1200, maxHeight:800}} id="videoID"></video>
+            <video controls={false} style={{maxWidth:1200, maxHeight:800, width:1200, height:800}} id="videoID"></video>
         </div>
     )
 }
